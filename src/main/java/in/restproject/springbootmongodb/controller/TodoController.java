43ef4ecb807c9faf3b5653f563b5d4d3d0a3d914
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2023-04-07 11:06:53 +0530

    Created rest endpoint to retrieve a single document from the db

--- src/main/java/in/restproject/springbootmongodb/controller/TodoController.java
+++ src/main/java/in/restproject/springbootmongodb/controller/TodoController.java
@@ -1,11 +1,16 @@
 package in.restproject.springbootmongodb.controller;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.restproject.springbootmongodb.repository.TodoRepository;
@@ -25,5 +30,41 @@ public class TodoController {
             return new ResponseEntity<>("No Todos available", HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping("/todos")
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todo) {
+        try {
+            todo.setCreatedAt(new Date(System.currentTimeMillis()));
+            todoRepo.save(todo);
+            return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/todos/{id}")
+    public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id) {
+        Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+        if (todoOptional.isPresent()) {
+            return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Todo not found with id" + id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }
